bc5655dfe221779c988ff7f16356e9d96cd4104a
Yhair Alexander <yhairalexanderl@gmail.com>
2020-01-25 17:43:44 -0500

    37. Safe storage of secrets

--- Angular8/DatingApp/DatingApp.API/obj/Debug/netcoreapp3.1/DatingApp.API.AssemblyInfo.cs
+++ Angular8/DatingApp/DatingApp.API/obj/Debug/netcoreapp3.1/DatingApp.API.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("57A8334D-DFE7-414C-9227-7DEFF5DAEBE2")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("DatingApp.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
